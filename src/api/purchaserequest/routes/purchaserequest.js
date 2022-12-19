5cdd4126fb838ba6f47f9b274dd2394ef7209760
Brook Belihu <68384400+brookbdt@users.noreply.github.com>
2022-12-19 11:35:57 +0300

    additional content-types

---
+++ src/api/purchaserequest/routes/purchaserequest.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * purchaserequest router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::purchaserequest.purchaserequest');
---
+++ src/api/purchaserequest/services/purchaserequest.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * purchaserequest service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::purchaserequest.purchaserequest');
