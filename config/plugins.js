f64484e2917b123238cb977401d461b5eeee04ee
Brook Belihu <68384400+brookbdt@users.noreply.github.com>
2023-02-14 11:57:44 +0300

    cloudinary config

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
+      provider: "cloudinary",
       providerOptions: {
-        localServer: {
-          maxage: 300000,
-        },
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
   },
