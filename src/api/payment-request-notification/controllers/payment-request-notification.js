2f9f0af458b007bc0a239dbdadbc27b5a10aa8d9
Brook Belihu <68384400+brookbdt@users.noreply.github.com>
2023-04-04 23:26:00 +0300

    major changes

---
+++ src/api/payment-request-notification/controllers/payment-request-notification.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * payment-request-notification controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::payment-request-notification.payment-request-notification');
---
+++ src/api/payment-request-notification/routes/payment-request-notification.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * payment-request-notification router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::payment-request-notification.payment-request-notification');
